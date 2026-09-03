d732dd26b826c81694c810f4be3027ac0a75608c
Mpho Molelle <mrockana@gmail.com>
2026-09-03 09:51:48 +0200

    Setup Serilog for structured logging (#7)
    * Setup Serilog for structured logging
    
    * Final Update for Serilog

--- GamesGlobal.ShoppingList.WebApi/DependencyInjectionExtensions.cs
+++ GamesGlobal.ShoppingList.WebApi/DependencyInjectionExtensions.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Sinks.OpenTelemetry;
 using static GamesGlobal.ShoppingList.WebApi.Common.WebApiConstants;
 using CommomConstants = GamesGlobal.ShoppingList.Application.Common.Constants;
 
@@ -13,10 +18,33 @@ namespace GamesGlobal.ShoppingList.WebApi;
 
 internal static class DependencyInjectionExtensions
 {
+    internal static void ConfigureSerilog(this ConfigureHostBuilder hostBuilder, IConfiguration configuration, bool isDevelopment = true)
+    {
+        string otelExportUrl = configuration.GetValue<string>("OtelExport:Endpoint") ?? string.Empty;
+
+        hostBuilder.UseSerilog((context, serviceProvider, config) =>
+        {
+            config.Enrich.FromLogContext();
+            config.Enrich.WithExceptionDetails();
+            config.WriteTo.Console();
+            config.ReadFrom.Configuration(context.Configuration);
+
+            config.WriteTo.OpenTelemetry(options =>
+            {
+                options.LogsEndpoint = otelExportUrl;
+                options.ResourceAttributes.Add("service.name", $"{CommomConstants.ApplicationName}_LOGS");
+            });
+
+            if (isDevelopment)
+            {
+                config.WriteTo.File("../appData/logs/log.txt", rollingInterval: RollingInterval.Day);
+            }
+        });
+    }
+
     internal static void SetupOpenTelemetryLogging(this ILoggingBuilder loggingBuilder)
     {
-        loggingBuilder.ClearProviders()
-            .AddConsole()
+        loggingBuilder
             .AddDebug()
             .AddOpenTelemetry(options =>
             {
@@ -24,12 +52,15 @@ internal static class DependencyInjectionExtensions
                 .SetResourceBuilder(ResourceBuilder.CreateDefault()
                 .AddService(CommomConstants.ApplicationName))
                 .IncludeScopes = true;
+
+                options.IncludeFormattedMessage = true;
+                options.ParseStateValues = true;
             });
     }
 
     internal static void AddOpenTelemetryServices(this IServiceCollection services, IConfiguration configuration, bool isDevelopment = true)
     {
-        string otelExportUrl = configuration["OtelExport:Endpoint"] ?? string.Empty;
+        string otelExportUrl = configuration.GetValue<string>("OtelExport:Endpoint") ?? string.Empty;
         var resourceBuilder = ResourceBuilder.CreateDefault()
                 .AddService(CommomConstants.ApplicationName);
 
@@ -66,13 +97,6 @@ internal static class DependencyInjectionExtensions
                 if (isDevelopment)
                 {
                     builder.AddConsoleExporter();
-                }
-            }).WithLogging(builder =>
-            {
-                if (isDevelopment)
-                {
-                    builder.AddConsoleExporter()
-                    .AddOtlpExporter(options => { options.Endpoint = new Uri(uriString: otelExportUrl); });
                 }
             });
     }
