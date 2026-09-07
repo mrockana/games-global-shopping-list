0f2446b50e913cb0b75ab81079466e4355c74f00
Mpho Molelle <mrockana@gmail.com>
2026-09-07 06:49:53 +0200

    Update Search Code

---
+++ GamesGlobal.ShoppingList.Application/Common/ShoppingItemsOptions.cs
@@ -0,0 +1,8 @@
+namespace GamesGlobal.ShoppingList.Application.Common;
+
+public sealed class ShoppingItemsOptions
+{
+    public double MinimumConfidence { get; init; }
+
+    public bool EnableVectorSearch { get; init; }
+}
